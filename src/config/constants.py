9ce1637fffc96e8626b14d65aeb71778e63feb9b
adick-kli <adick60@gmail.com>
2026-04-06 12:32:09 +0200

    Add constants for colors, fonts, device configuration, tool paths, and defaults

---
+++ src/config/constants.py
@@ -0,0 +1,31 @@
+# Constants for Colors
+
+# Define color constants
+PRIMARY_COLOR = '#3498db'
+SECONDARY_COLOR = '#2ecc71'
+ERROR_COLOR = '#e74c3c'
+WARNING_COLOR = '#f39c12'
+
+# Constants for Fonts
+
+# Define font constants
+DEFAULT_FONT = 'Arial'
+HEADER_FONT = 'Helvetica'
+FOOTER_FONT = 'Courier'
+
+# Device Configuration Constants
+
+# Define device configuration constants
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+DEVICE_NAME = 'MyDevice'
+
+# Tool Paths
+
+# Define tool paths
+TOOL_PATH_1 = '/usr/local/bin/tool1'
+TOOL_PATH_2 = '/usr/local/bin/tool2'
+
+# Default Values
+DEFAULT_TIMEOUT = 30
+DEFAULT_RETRIES = 3
